1889c4a7c0d43b436ab113a609870746de0ea472
Arthur Correnson <arthur.correnson@gmail.com>
2018-12-25 20:00:51 +0100

    Add basic input system

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+  // ask for degree of the polynomial
+  int n = 0;
+  printf("Enter the degree of the polynomial :\n");
+  scanf("%d", &n);
+  
+  // alloc memory to store coefs
+  int *coefs = malloc(n*sizeof(int));
+
+  // input coefs
+  for (int i = 0; i < n; ++i) {
+    printf("coefficient %d :\n", i);
+    scanf("%d", coefs+i);
+  }
+
+  printf("Your polynomial is :\n");
+
+  for (int i = 0; i < n; ++i) {
+    int c = coefs[i];
+    if (i == 0 && c != 0)
+      printf("%d + ", c);
+    else if (i < n-1 && c != 0)
+      printf("%dx^%d + ", c, i);
+    else if (c != 0)
+      printf("%dx^%d\n", c, i);
+  }
+  
+  return 0;
+}
