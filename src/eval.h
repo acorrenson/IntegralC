3c86ef3ab8354e22a85df64164744a3e27985772
Arthur Correnson <arthur.correnson@gmail.com>
2018-12-26 01:05:39 +0100

    Imrpove headers

--- src/eval.h
+++ src/eval.h
@@ -1,2 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 float eval(int *c, int n, float x);
+
+#endif
--- src/limit_sum.h
+++ src/limit_sum.h
@@ -1,2 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 double limit_sum_inf(int * c, int n, float a, float b);
+
+#endif
